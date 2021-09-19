5d46a9bdeaaa089403b123c47d2411ca5d710b00
xlsama <im.xlsama@gmail.com>
2021-09-19 10:42:16 +0800

    no message

--- node/node-server-01/index.ts
+++ node/node-server-01/index.ts
@@ -1,13 +1,14 @@
 import * as http from 'http'
+import {IncomingMessage, ServerResponse} from "http";
 
 const server = http.createServer()
 
-server.on('request', (request: http.IncomingMessage, response: http.ServerResponse) => {
+server.on('request', (request:IncomingMessage, response: ServerResponse) => {
     console.log('method:', request.method)
     console.log('url:', request.url)
     console.log('headers:', request.headers)
 
-    response.end('end')
+    response.end('hi')
 })
 
 server.listen(8888)
